56b9e2f99554cf9a5fc55c6f2b7b926f19777585
SiddheshNan <siddhesh.nandurkar@gmail.com>
2020-02-01 23:25:31 +0530

    v2

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,10 +1,13 @@
 import pyttsx3
 
 class TextToSpeech:
+    def __init__(self):
+        pass
+
     def speak(self, text):
         engine = pyttsx3.init()
         self.eng = engine
-        engine.setProperty('rate', 165)
+        engine.setProperty('rate', 150)
         engine.setProperty('volume',1.0)
         #voices = engine.getProperty('voices')
         #engine.setProperty('voice', voices[10].id)
