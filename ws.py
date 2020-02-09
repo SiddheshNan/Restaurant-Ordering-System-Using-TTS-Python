fd7979cab19fb7cfb56ba586c98161c02c0bbaad
Siddhesh <37670217+SiddheshNan@users.noreply.github.com>
2020-02-09 12:17:04 +0530

    add try catch in ws

--- ws.py
+++ ws.py
@@ -3,9 +3,13 @@ import websocket
 
 class Sockets:
     def __init__(self):
-        self.ws = websocket.WebSocket()
-        self.ws.connect("ws://localhost:8888/ws")
-        self.ws.send('{"SERVER_INFO":"payment system connected to WS"}')
+        try:
+            self.ws = websocket.WebSocket()
+            self.ws.connect("ws://localhost:8888/ws")
+            self.ws.send('{"SERVER_INFO":"payment system connected to WS"}')
+        except:
+            print("An WS exception occurred")
+            exit()
 
     def send(self, dataText):
         self.ws.send(dataText)
